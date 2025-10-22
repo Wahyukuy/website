3275f63234752247c5d6bc53053e0e16f194d5cb
Wahyukuy <105117758+Wahyukuy@users.noreply.github.com>
2025-10-22 11:36:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function tampilkanDeskripsi(id) {
   const deskripsi = {
     produk1: "Produk 1 adalah sepatu olahraga ringan dan nyaman.",
     produk2: "Produk 2 adalah tas ransel anti air dengan banyak kompartemen."
+    produk3: "Produk 4 adalah tas ransel anti air dengan banyak kompartemen."
+    produk4: "Produk 4 adalah tas ransel anti air dengan banyak kompartemen."
   };
   document.getElementById("deskripsi-produk").innerText = deskripsi[id];
 }
